8983f568be7fc126a7d9024c8752e6e5fe84bd7b
WangGaoJie <kinghd1912@163.com>
2024-08-20 10:32:25 +0800

    Update copyright information

--- pt_plus.c
+++ pt_plus.c
@@ -1,7 +1,7 @@
 /* 
  * The MIT License (MIT)
  *
- * Copyright (c) 2023, DazzlingOkami (https://dazzlingokami.github.io/)
+ * Copyright (c) 2024, DazzlingOkami (https://dazzlingokami.github.io/)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
